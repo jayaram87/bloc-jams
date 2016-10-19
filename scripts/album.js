c32f72833e8156c6445bd43f9c86cd5efbad68e7
Jayaram Raja <jayaramraja1987@gmail.com>
2016-10-19 02:28:06 +0000

    assignment-25

--- scripts/album.js
+++ scripts/album.js
@@ -28,6 +28,21 @@ var albumMarconi = {
     ]
 };
 
+var albumAerosmith = {
+    title: 'Aerosmith',
+    artist: 'Aerosmith',
+    label: 'EM',
+    year: '1976',
+    albumArtUrl: 'assets/images/album_covers/22.png',
+    songs: [
+        { title: 'Hello, Operator?', duration: '1:01' },
+        { title: 'Ring, ring, ring', duration: '5:01' },
+        { title: 'Fits in your pocket', duration: '3:21'},
+        { title: 'Can you hear me now?', duration: '3:14' },
+        { title: 'Wrong phone number', duration: '2:15'}
+    ]
+};
+
 var createSongRow = function(songNumber, songName, songLength) {
     var template =
         '<tr class="album-view-song-item">'
@@ -39,14 +54,16 @@ var createSongRow = function(songNumber, songName, songLength) {
     return template;
 };
 
+//global variable
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+
 var setCurrentAlbum = function(album) {
     
     var albumTitle = document.getElementsByClassName('album-view-title')[0];
     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-    var albumImage = document.getElementsByClassName('album-cover-art')[0];
     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
- 
+    
     albumTitle.firstChild.nodeValue = album.title;
     albumArtist.firstChild.nodeValue = album.artist;
     albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
@@ -58,7 +75,19 @@ var setCurrentAlbum = function(album) {
         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
     }
 };
- 
+
 window.onload = function() {
+
     setCurrentAlbum(albumPicasso);
+    
+    var albumList = [albumPicasso, albumMarconi, albumAerosmith];
+    var i = 0;
+    
+    albumImage.addEventListener('click',function(event){
+        setCurrentAlbum(albumList[i]);
+        i++;
+        if(i == albumList.length){
+            i = 0; // resets the album to the 1st album
+        }
+    });
 };
