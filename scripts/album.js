a378c4071563a1bfffb01bee64745d0cd9905058
Jayaram Raja <jayaramraja1987@gmail.com>
2016-11-11 05:46:23 +0000

    assignment-34

--- scripts/album.js
+++ scripts/album.js
@@ -3,7 +3,7 @@ var createSongRow = function(songNumber, songName, songLength) {
         '<tr class="album-view-song-item">'
             +'<td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
             +'<td class="song-item-title">' + songName + '</td>'
-            +'<td class="song-item-duration">' + songLength + '</td>'
+            +'<td class="song-item-duration">' + filterTimeCode(songLength) + '</td>'
         +'</tr>'
     ;
     
@@ -30,6 +30,7 @@ var createSongRow = function(songNumber, songName, songLength) {
             $volumeThumb.css({left: currentVolume + '%'});
 		    
 		    updatePlayerBarSong();
+		    
 	    } else if (currentlyPlayingSongNumber === songNumber) {
 		    if (currentSoundFile.isPaused()) {
                 $(this).html(pauseButtonTemplate);
@@ -98,6 +99,8 @@ var updateSeekBarWhileSongPlays = function() {
             var $seekBar = $('.seek-control .seek-bar');
  
             updateSeekPercentage($seekBar, seekBarFillRatio);
+            setCurrentTimeInPlayerBar(filterTimeCode(this.getTime()));
+            setTotalTimeInPlayerBar(filterTimeCode(this.getDuration()));
         });
     }
 };
@@ -238,7 +241,7 @@ var updatePlayerBarSong = function() {
     $('.currently-playing .artist-name').text(currentAlbum.artist);
     $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.title + " - " + currentAlbum.artist);
     $('.main-controls .play-pause').html(playerBarPauseButton);
-
+    
 };
 
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>'
@@ -289,6 +292,21 @@ var setVolume = function(volume) {
     }
 };
 
+var setCurrentTimeInPlayerBar = function(time){
+    $('.current-time').text(time);    
+};
+
+var setTotalTimeInPlayerBar = function(totalTime){
+    $('.total-time').text(totalTime);    
+};
+
+var filterTimeCode = function(timeInSeconds){
+    var totalSeconds = parseFloat(timeInSeconds);
+    var minutes = Math.floor(totalSeconds / 60);
+    var seconds = Math.floor(totalSeconds % 60);
+    
+    return minutes + ":" + seconds;
+};
 
 $(document).ready(function() {
     setCurrentAlbum(albumPicasso);
